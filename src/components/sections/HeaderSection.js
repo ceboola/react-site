e9637b4c0b99cd5c178ff6ebfad8d3e3a346a4db
ceboola <mrcarrr@gmail.com>
2017-06-17 17:44:16 +0200

    Initial commit

---
+++ src/components/sections/HeaderSection.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Grid, Col, Row } from 'react-bootstrap';
+
+export const HeaderSection =  class extends Component {
+  render() {
+    return (
+    <Grid fluid={true} className="App-header">
+        <Row>
+          <Col xs={12} md={12}>
+            <div className="jumbo-text"><h2 className="h2">react is<br/>awesome<br/><p className="underline-hero">library</p></h2>
+              <p className="header-text">You should learn it too!</p>
+            </div>
+          </Col>
+        </Row>
+    </Grid>
+)
+  }
+}
